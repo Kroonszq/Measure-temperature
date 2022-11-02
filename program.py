32e7c4a8227c9189a9799ba665b5c396ead98e50
Kroonsz <Kroonszq@gmail.com>
2022-11-02 16:15:05 +0100

    upload data

--- program.py
+++ program.py
@@ -1,13 +1,41 @@
 import requests
+import time
+import random
+import json
 
 
-URL = "https://api.agify.io?name=meelad"
-PARAMS = {"name": "meelad"}
-response = requests.get(URL, PARAMS)
+URL: str = "https://api.basecampserver.tech/sensors?key=Gl1AyMQ2tguwtV7bAMR8oQ"
 
-if response.status_code != 200:
-    exit("Something went wrong")
+def uploadData(timestamp, temperature, humidity, pressure):
+    DATA = {
+        "timestamp": round(int(timestamp),2),
+        "temperature": round(temperature,2),
+        "humidity": round(humidity,2),
+        "pressure": round(pressure,2),
+    }
+
+    print(DATA)
+
+    while True:
+        try:
+            res = requests.post(url=URL, data=json.dumps(DATA), headers={"Content-Type": "application/json", "accept": "application/json"})
+            print(f"Data uploaded to the server")
+            print(res.status_code)
+            break
+        except requests.exceptions.Timeout as e:
+            print(e)
+            continue
+        except requests.exceptions.HTTPError as x:
+            print(x)
+            exit("Something went terribly wrong :(")
+
+def main():
+    while True:
+        uploadData(time.time(), random.randint(3, 12),  random.randint(3, 12),  random.randint(3, 13))
+        time.sleep(10)
 
 
 
+if __name__ == "__main__":
+    main()
 
